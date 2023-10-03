97d995f8be956f0328f631f50c9dd07a14147370
Dave Naugler <davenaugler@gmail.com>
2023-10-03 14:48:21 -0600

    Working on creating config file for constants/final

---
+++ src/config/SalesDataAppConfig.java
@@ -0,0 +1,17 @@
+package config;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class SalesDataAppConfig {
+	
+	//TeslaModel.java
+//	public final String DISPLAYNAME;
+//    public final String CSVFILENAME;
+	
+	// DateService.java
+//	public static final DateTimeFormatter MONTH_FORMATTER = DateTimeFormatter.ofPattern("MMM-yy", Locale.US);
+//    public static final DateTimeFormatter YEAR_MONTH_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM");
+
+
+}
--- src/services/DateService.java
+++ src/services/DateService.java
@@ -5,14 +5,21 @@ import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+
 public class DateService {
 
     private static final DateTimeFormatter MONTH_FORMATTER = DateTimeFormatter.ofPattern("MMM-yy", Locale.US);
     private static final DateTimeFormatter YEAR_MONTH_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM");
+    
+    
 
     public static Month parseMonthFromCSV(String dateString) {
         return Month.from(MONTH_FORMATTER.parse(dateString));
     }
+	
+//	public static Month parseMonthFromCSV(String dateString) {
+//        return Month.from(config.SalesDataAppConfig.MONTH_FORMATTER.parse(dateString));
+//    }
 
     public static Integer parseYearFromCSV(String dateString) {
         int twoDigitYear = Integer.parseInt(dateString.split("-")[1]);
@@ -23,5 +30,10 @@ public class DateService {
         LocalDate date = LocalDate.of(year, month, 1);
         return date.format(YEAR_MONTH_FORMATTER);
     }
+    
+//    public static String formatMonthYear(Month month, Integer year) {
+//        LocalDate date = LocalDate.of(year, month, 1);
+//        return date.format(config.SalesDataAppConfig.YEAR_MONTH_FORMATTER);
+//    }
 
 }
